7fc8b29c7cf5a6f064725e73959bbceeb18695ac
Arjit184 <arjitpathak805@gmail.com>
2026-03-28 11:19:00 +0530

    Add translation_agent function for Hinglish conversion

---
+++ translation.py
@@ -0,0 +1,6 @@
+from utils import call_model
+
+def translation_agent(text):
+    prompt = f"Convert this into Hinglish:\n{text}"
+    res = call_model([{"role": "user", "content": prompt}])
+    return res.choices[0].message.content.strip()
